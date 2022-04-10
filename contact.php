6356d9c937bc37f1a4e8081230ca0b542143a2e4
Roshanyadav1324 <62843947+Roshanyadav1324@users.noreply.github.com>
2022-04-10 13:22:30 +0530

    First Commit

---
+++ contact.php
@@ -0,0 +1,30 @@
+<section class="contact">
+    <div class="container mt-4" id="Contact">  
+      <!--Start Contact Us Container-->
+          <h2 class="text-center mb-4">Contact US</h2> <!-- Contact Us Heading -->
+          <hr>
+          <br>
+          <br>
+          <div class="row">  <!--Start Contact Us Row-->
+            <div class="col-md-8"> <!--Start Contact Us 1st Column-->
+              <form action="" method="post" class="forms">
+                <input type="text" class="form-control" name="name" placeholder="Name"><br>
+                <input type="text" class="form-control" name="subject" placeholder="Subject"><br>
+                <input type="email" class="form-control" name="email" placeholder="E-mail"><br>
+                <textarea class="form-control" name="message" placeholder="How can we help you?" style="height:150px;"></textarea><br>
+                <input class="btn btn-primary" type="submit" value="Send" name="submit"><br><br>
+              </form>
+            </div> <!-- End Contact Us 1st Column-->
+    
+             <!-- Start Contact Us 2nd Column-->
+            <div class="col-md-4 stripe text-white text-center"> 
+              <h4>Invictus Educare</h4>
+              <p> Caption Complex, Near City Hospital, 
+              Naka No: 5, Darbhanga - 84604<br /> 
+              Phone: 9852322566 <br />
+              www.invictuseducare.com </p>
+            </div> <!-- End Contact Us 2nd Column-->
+          </div> <!-- End Contact Us Row-->
+        </div> <!-- End Contact Us Container-->
+        <!-- End Contact Us -->
+    </section>
---
+++ mainInclude/header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    
+     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" type="text/css" href="css/all.min.css">
+
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+ <link href="https://fonts.googleapis.com/css2?family=Smokum&display=swap" rel="stylesheet">
+
+    <!-- Student Testimonial Owl Slider CSS -->
+    <link rel="stylesheet" type="text/css" href="css/owl.min.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.theme.min.css">
+    <link rel="stylesheet" type="text/css" href="css/testyslider.css">
+
+    <!-- Custom Style CSS -->
+    <link rel="stylesheet" type="text/css" href="./css/styles.css" />
+    <title>Invictus</title>
+  </head>
+  <body>
+     <!-- Start Nagigation -->
+    <nav class="navbar navbar-expand-sm navbar-dark pl-5 bg-dark fixed-top">
+    <div class="container-fluid">
+  <img  src="image/logo.png" alt="logo" style="width:70px;height:70px; padding-right: 10px;">
+  <a class="navbar-brand " href="index.php">INVITCUS Educare</a>
+      <span class="navbar-text">Educare</span>
+      <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myMenu">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="myMenu">
+        <ul class="navbar-nav pl-5 custom-nav">
+          <li class="nav-item custom-nav-item"><a href="index.php" class="nav-link">Home</a></li>
+          <li class="nav-item custom-nav-item"><a href="courses.php" class="nav-link">Courses</a></li>
+          <li class="nav-item custom-nav-item"><a href="paymentstatus.php" class="nav-link">Payment Status</a></li>
+          <?php 
+              session_start();   
+              if (isset($_SESSION['is_login'])){
+                echo '<li class="nav-item custom-nav-item"><a href="student/studentProfile.php" class="nav-link">My Profile</a></li> <li class="nav-item custom-nav-item"><a href="logout.php" class="nav-link">Logout</a></li>';
+              } else {
+                echo '<li class="nav-item custom-nav-item"><a href="#login" class="nav-link" data-toggle="modal" data-target="#stuLoginModalCenter">Login</a></li> <li class="nav-item custom-nav-item"><a href="#signup" class="nav-link" data-toggle="modal" data-target="#stuRegModalCenter">Signup</a></li>';
+              }
+          ?> 
+          <!--<li class="nav-item custom-nav-item"><a href="#Feedback" class="nav-link">Feedback</a></li> -->
+          <li class="nav-item custom-nav-item"><a href="#Contact" class="nav-link">Contact</a></li>
+        </ul>
+      </div>
+            </div>
+    </nav> <!-- End Navigation -->
